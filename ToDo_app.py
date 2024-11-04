564b384f64d8dcd29c9ae02ff05c07b7971101e0
alfredd25 <alfredbiju7@gmail.com>
2024-11-04 10:19:38 +0530

    Add edit and delete functions

--- ToDo_app.py
+++ ToDo_app.py
@@ -1,11 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 class TodoApp:
     def __init__(self, root):
-        self.task_listbox = None
-        self.add_task_button = None
-        self.task_entry = None
         self.root = root
         self.root.title("To-Do List")
         self.root.geometry("400x400")
@@ -24,6 +22,12 @@ class TodoApp:
         self.task_listbox = tk.Listbox(self.root, width=50, height=10)
         self.task_listbox.pack(pady=10)
 
+        self.edit_tasks = tk.Button(self.root, text="Edit Task", command=self.edit_task)
+        self.edit_tasks.pack(pady=5)
+
+        self.delete_tasks = tk.Button(self.root, text="Delete Task", command=self.delete_task)
+        self.delete_tasks.pack(pady=5)
+
     def add_task(self):
         task = self.task_entry.get()
         if task:
@@ -32,6 +36,25 @@ class TodoApp:
             self.task_entry.delete(0, tk.END)
         else:
             messagebox.showwarning("Warning", "Please enter a task.")
+
+    def edit_task(self):
+        try:
+            index = self.task_listbox.curselection()[0];
+            selected_task = self.tasks[index]
+            self.task_entry.delete(0, tk.END)
+            self.task_entry.insert(0, selected_task)
+            self.delete_task()
+
+        except IndexError:
+            messagebox.showwarning("Warning", "Please select a task to edit")
+
+    def delete_task(self):
+        try:
+            index = self.task_listbox.curselection()[0]
+            self.tasks.pop(index)
+            self.update_task_list()
+        except IndexError:
+            messagebox.showwarning("Warning", "Please select a task to delete")
 
     def update_task_list(self):
         self.task_listbox.delete(0, tk.END)
